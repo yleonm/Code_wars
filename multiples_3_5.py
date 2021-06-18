fe26b252c79f494428d29e7d2238eb05b6295aad
Yerson León <yelemo777@gmail.com>
2021-06-18 15:33:27 -0500

    Inicio reto (kata) multiplos de 3 y de 5

---
+++ multiples_3_5.py
@@ -0,0 +1,18 @@
+"""If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number
+ passed in.
+
+Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is 
+negative, return 0(for languages that do have them)
+"""
+
+def solution(number):
+    if number < 0:
+        return 0
+    elif number >=0:
+         lista = [x for x in range(0,number+1)]
+    return lista
+
+print(solution(35))
